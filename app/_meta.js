231ddd59550ec7f3a61789d285fb8cc642535c4c
CodeByNikolas <Nikolas.Hack@tum.de>
2025-11-25 16:47:07 +0100

    Good boilerplate for now

--- app/_meta.js
+++ app/_meta.js
@@ -1,12 +1,18 @@
 export default {
  "introduction": "Introduction",
-  "resources": "Resources",
-  "tutorials": "Tutorials",
-  "education": "Education",
-  "it-and-development": "IT and Development",
-  "community": "Community",
-  "legal-and-finance": "Legal and Finance",
-  "marketing": "Marketing",
-  "industry": "Industry",
-  "external-affairs": "External Affairs"
+//   "resources": "Resources",
+//   "tutorials": "Tutorials",
+//   "education": "Education",
+//   "it-and-development": "IT and Development",
+//   "community": "Community",
+//   "legal-and-finance": "Legal and Finance",
+//   "marketing": "Marketing",
+//   "industry": "Industry",
+//   "external-affairs": "External Affairs"
 } 
+
+
+// export default {
+//   "introduction": "Intro",
+//   "industry": "Industry"
+// }
